2239916ada510afcb2f7c17ea03f9e96fe6b853f
Ha1zz <earth272272@gmail.com>
2020-11-20 20:25:54 -0500

    Initial commit

---
+++ Assets/Scripts/ChangeSceneScript.cs
@@ -0,0 +1,31 @@
+﻿//Name: Tran Thien Phu
+//ID: 101160213
+//Date Last Modifield: 20/11/2020
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ChangeSceneScript : MonoBehaviour
+{
+    private string sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
